ab75bf50a3b557f8e35a69e23423c928985cf2a4
kgithcoy <anhtuannguyen.2004@gmail.com>
2023-03-30 13:42:57 +0200

    CertificateDAO toegevoegd

---
+++ codecademy/src/main/java/com/codecademy/dao/CertificateDAO.java
@@ -0,0 +1,12 @@
+package com.codecademy.dao;
+
+import com.codecademy.domain.Certificate;
+import javafx.collections.ObservableList;
+
+
+public interface CertificateDAO {
+    ObservableList<Certificate> getCertificates();
+    void addCertificate(Certificate certificate);
+    void updateCertificate(Certificate certificate);
+    void deleteCertificate(Certificate certificate);
+}
---
+++ codecademy/src/main/java/com/codecademy/dao/CertificateDAOimpl.java
@@ -0,0 +1,33 @@
+package com.codecademy.dao;
+
+import com.codecademy.domain.Certificate;
+
+import javafx.collections.ObservableList;
+
+public class CertificateDAOimpl implements CertificateDAO{
+
+    @Override
+    public ObservableList<Certificate> getCertificates() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCertificates'");
+    }
+
+    @Override
+    public void addCertificate(Certificate certificate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addCertificate'");
+    }
+
+    @Override
+    public void updateCertificate(Certificate certificate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCertificate'");
+    }
+
+    @Override
+    public void deleteCertificate(Certificate certificate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteCertificate'");
+    }
+    
+}
